c82a5fa0d9359d893972a6a41a72bc5c9b1b1a0e
akiran <kiran@neostack.com>
2021-02-02 22:00:02 +0530

    toggle todogs

--- src/Todos/Todo.js
+++ src/Todos/Todo.js
@@ -1,7 +1,7 @@
 import graphql from "babel-plugin-relay/macro";
 import { useFragment } from "react-relay/hooks";
 import DeleteTodo from "./DeleteTodo";
-// import ToggleTodo from "./ToggleTodo";
+import ToggleTodo from "./ToggleTodo";
 
 const fragment = graphql`
   fragment Todo_Query on todos {
@@ -15,7 +15,7 @@ function Todo({ todo }) {
   const data = useFragment(fragment, todo);
   return (
     <li>
-      {/* <ToggleTodo id={data.id} completed={data.completed} /> */}
+      <ToggleTodo id={data.id} completed={data.completed} />
       <label>{data.title}</label>
       <DeleteTodo id={todo.id} />
     </li>
--- src/Todos/ToggleTodo.js
+++ src/Todos/ToggleTodo.js
@@ -1,39 +1,44 @@
-// import { useMutation } from "react-relay/hooks";
-// import graphql from "babel-plugin-relay/macro";
+import { useMutation } from "react-relay/hooks";
+import graphql from "babel-plugin-relay/macro";
+import { fromGlobalId } from "graphql-relay";
 
-// const toggleTodoMutation = graphql`
-//   mutation ToggleTodo_Mutation($input: ToggleTodoInput!) {
-//     toggleTodo(input: $input) {
-//       todo {
-//         id
-//         completed
-//       }
-//     }
-//   }
-// `;
+const toggleTodoMutation = graphql`
+  mutation ToggleTodo_Mutation(
+    $set: todos_set_input
+    $pk_columns: todos_pk_columns_input!
+  ) {
+    update_todos_by_pk(_set: $set, pk_columns: $pk_columns) {
+      id
+      completed
+    }
+  }
+`;
 
-// export default function ToggleTodo({ id, completed }) {
-//   console.log(id, completed);
-//   const [commit, isInFlight] = useMutation(toggleTodoMutation);
-//   function changeHandler(e) {
-//     console.log(e.target.checked);
-//     commit({
-//       variables: {
-//         input: {
-//           todoId: id,
-//         },
-//       },
-//       onCompleted(data) {
-//         console.log(data, "!!!");
-//       },
-//     });
-//   }
-//   return (
-//     <input
-//       className="toggle"
-//       type="checkbox"
-//       checked={completed}
-//       onChange={changeHandler}
-//     ></input>
-//   );
-// }
+export default function ToggleTodo({ id, completed }) {
+  console.log(id, completed);
+  const [commit, isInFlight] = useMutation(toggleTodoMutation);
+  function changeHandler(e) {
+    console.log(e.target.checked);
+    const gid = fromGlobalId(id);
+    const uuid = JSON.parse(gid.id)[3];
+    commit({
+      variables: {
+        set: {
+          completed: e.target.checked,
+        },
+        pk_columns: { id: uuid },
+      },
+      onCompleted(data) {
+        console.log(data, "!!!");
+      },
+    });
+  }
+  return (
+    <input
+      className="toggle"
+      type="checkbox"
+      checked={completed}
+      onChange={changeHandler}
+    ></input>
+  );
+}
